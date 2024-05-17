d234b2a085738860470649cc5bf6d4654567926c
Tarun-84 <tarun17nxt@gmail.com>
2024-05-17 12:18:35 +0530

    banner heading css

--- src/pages/Home/HomeBanner.jsx
+++ src/pages/Home/HomeBanner.jsx
@@ -15,7 +15,7 @@ function HomeBanner() {
     <>
       <div className="home-banner p-10 flex flex-col justify-between">
         <div className="flex flex-col justify-center md:items-baseline items-center gap-y-8 banner-txt">
-          <h1 className="text-white xl:text-7xl lg:text-6xl lg:leading-[5rem] text-[30px] text-center md:text-left ">
+          <h1 className="text-white xl:text-8xl lg:text-6xl lg:leading-[5rem] text-[30px] text-center md:text-left ">
             Experience the thrill <br />
             of the road with our
             <br /> top-notch cars!"
@@ -23,7 +23,7 @@ function HomeBanner() {
           <p className="text-white text-2xl text-center md:text-left w-full">
             Buy a quality second-hand car
           </p>
-          <button className="text-white banner-btn text-md py-2 px-4 taviraj">
+          <button className="text-white banner-btn text-md md:text-xl py-2 px-4 taviraj">
             {" "}
             Buy - +91 9264492644
           </button>
