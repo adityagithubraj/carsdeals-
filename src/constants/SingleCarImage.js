1eaf867ad2a0d0f6037003b1416ba0d24fe172f2
Tarun <tarun@thenightmarketer.com>
2024-05-21 11:59:22 +0530

    single product page image update

--- src/constants/SingleCarImage.js
+++ src/constants/SingleCarImage.js
@@ -5,7 +5,12 @@ import carImg4 from "../assets/images/carimages/carimg-1.png"
 import carImg5 from "../assets/images/carimages/carimg-2.png"
 import carImg6 from "../assets/images/carimages/carimg-3.png"
 
+import car1 from '../assets/images/carimages/single car-1.png'
+import car2 from '../assets/images/carimages/single car-2.png'
+import car3 from '../assets/images/carimages/single car-3.png'
+
 export const singleCarImage = [
     
-        carImg1 , carImg2 , carImg3 , carImg4 , carImg5 , carImg6
+        // carImg1 , carImg2 , carImg3 , carImg4 , carImg5 , carImg6
+        car1 , car2 ,car3
 ]
