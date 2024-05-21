15fd49ed3a4b62787809082230a0673613f209bb
Tarun <tarun@thenightmarketer.com>
2024-05-21 12:00:36 +0530

    single product page

--- src/constants/SingleCarImage.js
+++ src/constants/SingleCarImage.js
@@ -1,16 +1,7 @@
-import carImg1 from "../assets/images/carimages/carimg-1.png"
-import carImg2 from "../assets/images/carimages/carimg-2.png"
-import carImg3 from "../assets/images/carimages/carimg-3.png"
-import carImg4 from "../assets/images/carimages/carimg-1.png"
-import carImg5 from "../assets/images/carimages/carimg-2.png"
-import carImg6 from "../assets/images/carimages/carimg-3.png"
-
 import car1 from '../assets/images/carimages/single car-1.png'
 import car2 from '../assets/images/carimages/single car-2.png'
 import car3 from '../assets/images/carimages/single car-3.png'
 
 export const singleCarImage = [
-    
-        // carImg1 , carImg2 , carImg3 , carImg4 , carImg5 , carImg6
         car1 , car2 ,car3
 ]
